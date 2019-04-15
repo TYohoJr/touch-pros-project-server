e4b56aea00a024ef6edbfa7678f3ce3eba8d1a4c
Thomas Yoho <TYohoJr@Gmail.com>
2019-04-15 07:14:04 -0500

    missing period...

--- express.js
+++ express.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res,sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
