43f71480bb8c3771d4d4b4f4deb1d2718a52efa2
Thomas Yoho <TYohoJr@Gmail.com>
2019-04-15 07:10:30 -0500

    server not starting on heroku

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express();
+let app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json({ type: 'application/json' }));
